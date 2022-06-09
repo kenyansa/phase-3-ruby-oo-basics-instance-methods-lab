8a721e9e59330c8bb06b8cf90d3a14d72f4c93b4
Kenyansa <kenyansajustine@gmail.com>
2022-06-09 14:16:40 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+    def bark 
+        puts "Woof!"
+    end
+end
+fido = Dog.new
+fido.bark
+
+class Dog
+snoopy = Dog.new
+snoopy.bark
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+kansiime = Dog.new
+kansiime.sit
